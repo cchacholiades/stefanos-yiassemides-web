65b5a3357535eca23aa742c9a8777929b8ddcab5
Chris Chacholiades <chris.chacholiades@gmail.com>
2022-07-17 15:37:07 +0300

    fix(images): prefix url 3

--- components/Clients.vue
+++ components/Clients.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="text-center text-3xl sm:text-4xl font-bold mt-20 md:mt-32 mb-6">CLIENTS</div>
         <div class="grid grid-cols-3 sm:grid-cols-6 gap-4 flex items-center justify-center grayscale contrast-125">
-            <img class="invert" src="/clients/1a.png" alt="Client logo for Moonbug Entertainment"/>
+            <img class="invert" src="/stefanos-yiassemides-web/clients/1a.png" alt="Client logo for Moonbug Entertainment"/>
             <img src="/stefanos-yiassemides-web/clients/2.png" alt="Client logo for Lego" class="w-36 m-auto"/>
             <img src="/stefanos-yiassemides-web/clients/3.png" alt="Client logo for Fisherprice"/>
             <img src="/stefanos-yiassemides-web/clients/4.svg" alt="Client logo for Megabloks" class="w-32 m-auto"/>
