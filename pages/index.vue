4da5410ed309869f271b371fd269a5a8a1b2223a
Chris Chacholiades <chris.chacholiades@gmail.com>
2022-12-16 16:16:41 +0200

    fix(ui): z index issue

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="pb-10">
         <div class="relative h-96 md:h-max">
-            <div class="block flex justify-center items-center ">
+            <div class="absolute bottom-0 z-20 bg-gradient-to-t from-black to-transparent h-32 w-full"></div>
+
+            <div class="block flex justify-center items-center">
                 <img class="w-full h-96 md:h-max relative z-10 grayscale object-cover" src="/bg-2.jpeg" alt="Cover photo">
                 <Logo class="absolute z-20 top-0"/>
             </div>
-            <div class="absolute bottom-0 z-20 bg-gradient-to-t from-black to-transparent h-32 w-full"></div>
         </div>
 
         <Navigation class="fixed top-0 left-0 right-0 max-w-screen-lg z-40 bg-gradient-to-b from-black to-transparent m-auto" @goto="captureGoTo"/>
