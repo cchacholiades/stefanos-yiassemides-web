0e386a3aa88c9aa0e8498147f824ec9ae37ee126
Chris Chacholiades <chris.chacholiades@gmail.com>
2024-12-02 20:17:44 +0000

    feat(web): copy changes

--- components/About.vue
+++ components/About.vue
@@ -2,14 +2,17 @@
     <div class="mt-4">
         <div class="border-l-8 pl-4 border-solid border-yellow text-3xl sm:text-4xl font-bold mt-20 md:mt-32 mb-6 md:mb-10">ABOUT</div>
         <div class="md:flex text-lg md:text-xl">
-            <NuxtImg format="webp" src="/about-2.jpeg" class="w-full mb-6 md:w-56 md:h-56 m-auto mb-6 md:mb-0 md:mr-6 md:mt-0 md:ml-0 rounded-full object-cover grayscale max-w-[200px]" style="aspect-ratio: 1 / 1;" loading="lazy" alt="Stefanos Yiassemides self image" width="448" height="448"/>
+            <div class="md:w-56 md:h-56 flex-shrink-0">
+                <NuxtImg format="webp" src="/about-2.jpeg" class="w-full mb-6 m-auto mb-6 md:mb-0 md:mr-6 md:mt-0 md:ml-0 rounded-full object-cover grayscale max-w-[200px]" style="aspect-ratio: 1 / 1;" loading="lazy" alt="Stefanos Yiassemides self image" width="448" height="448"/>
+            </div>
             <div>
                 <div class="mb-4 break-words whitespace-pre-line">
-                    <v-clamp autoresize :max-lines="8">
-                    Stefanos Yiassemides is a Greek Cypriot composer and guitarist currently based in Bristol, UK. He took his first musical steps at the age of 12 and since then performing and writing music has been his lifelong passion. His versatility and attitude allow him to work on a wide variety of projects, regardless of genre, from pop songs and electronic music to full scale orchestral cinematic pieces. Stefanos has written music for award-winning global entertainment companies behind some of the most popular kids’ titles in the world, featuring his work on platforms such as Netflix, YouTube and Roku. He has worked on numerous multiplicity projects including video games, cartoons, animated series, short form and feature films.
+                    <v-clamp autoresize :max-lines="7">
+                        I'm a versatile music composer based in Bristol, UK, with a lifelong passion for music and film that began at an early age. I believe the synergy between music and visuals is one of the most beautiful art forms. Scoring for media felt like a natural path for me, and I find joy in crafting music that enhances visual storytelling, bringing each scene to life with every note.
 
-                    After studying under some of the most acclaimed music mentors in Cyprus, he went on to continue his studies in the UK. He earned his BA degree in Music Performance & Songwriting from the BIMM Institute Brighton in 2013 and a Master's degree in Music Composition and Film Scoring from the University of Bristol in 2017.
+                        I've had the privilege of writing music for award-winning global entertainment companies behind some of the most popular kids’ titles in the world. My work has been featured on platforms such as Netflix, YouTube, and Amazon. Over the years, I’ve worked on a variety of projects, including video games, cartoons, animated series, short-form content, and feature films.
 
+                        My journey began in Cyprus, where I studied under some of the most acclaimed music mentors. I later continued my education in the UK, earning a BA in Music Performance & Songwriting from the BIMM Institute Brighton in 2013, followed by a Master's in Music Composition and Film Scoring from the University of Bristol in 2017.
                     <button
                         v-if="expanded || clamped"
                         slot="after"
--- components/Testimonials.vue
+++ components/Testimonials.vue
@@ -3,12 +3,17 @@
         <div class="border-l-8 pl-4 border-solid border-yellow text-3xl sm:text-4xl font-bold mt-20 md:mt-32 mb-6 md:mb-10">TESTIMONIALS</div>
         <p class="mb-8 break-words text-lg md:text-xl italic">
             &ldquo;Stefanos is a fantastic composer to work with on projects. He is an extremely reliable, versatile and creative artist who responds very well to our musical briefs and feedback. He is a very versatile composer who can work effectively across our varied musical requirements from epic orchestral scores to melodic pop/rock songs&rdquo;
-            <span class="block mt-2 font-semibold text-sm not-italic">&#8211; Joshua E. - Senior Post Production Producer, Moonbug Entertainment</span>
+            <span class="block mt-2 font-semibold text-sm not-italic">&#8211; Joshua Embury - Music Director, Moonbug Entertainment</span>
+        </p>
+
+        <p class="mb-8 break-words text-lg md:text-xl italic">
+            &ldquo;A fantastic experience with Stefanos! His work truly elevated our project and his ability to understand our vision made the process seamless and enjoyable. His professionalism and responsiveness was really appreciated and I look forward to working together again in the future!&rdquo;
+            <span class="block mt-2 font-semibold text-sm not-italic">&#8211; Majda Al Maskati - Director &amp; Producer, Al Maskati Productions</span>
         </p>
 
         <p class="break-words text-lg md:text-xl italic">
             &ldquo;From the outset Stefanos worked hard to accommodate a constantly shifting design ethos, adapting to rough briefs with a creativity that enhanced the projects narrative elements. With each track he brought a distinct tone and emotional drive that underpinned the core themes of the narrative events they framed, and when change was needed he worked hard to accommodate the needs of the game with a friendly and easy to approach manner. If you need distinct, emotionally driven music, then there are few composers I could reccomend more.&rdquo;
-            <span class="block mt-2 font-semibold text-sm not-italic">&#8211; Nicholas P. - Game Developer, Mega Sloth Studios</span>
+            <span class="block mt-2 font-semibold text-sm not-italic">&#8211; Nick Pritchard - Game Developer, Mega Sloth Studios</span>
         </p>
     </div>
 </template>
